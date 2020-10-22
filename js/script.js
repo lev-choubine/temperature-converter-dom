fd97d1dcfea981f2e5c77c279950766c71429638
Lev Michael <lev.choubine@gmail.com>
2020-10-22 16:50:27 -0400

    [js][html][css] add skeleton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log('Hello, front end');
+const inputWindow = document.getElementsByTagName('input');
+const submitButton = document.querySelector('#submit');
+const screen = document.querySelector('.screen');
+console.log(inputWindow.value);
