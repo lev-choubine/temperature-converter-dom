6640018bedead22e4e8df36888361a7e7df02643
Lev Michael <lev.choubine@gmail.com>
2020-10-23 04:57:45 -0400

    [html [CSS][JS] built out the app

--- js/script.js
+++ js/script.js
@@ -1,4 +1,52 @@
-const inputWindow = document.getElementsByTagName('input');
-const submitButton = document.querySelector('#submit');
-const screen = document.querySelector('.screen');
-console.log(inputWindow.value);
+//this step will decide the direction of coversion based on the radioButton value
+
+let shotCaller;
+
+function radioFunction(browser) {
+  var shotCaller = browser;
+  console.log(shotCaller);
+  return shotCaller;
+} 
+// NOT ABLE TO PASS shotCaller value later in the programm -says "undefined" - would love to get some feedback as to why..
+// establish conversion funcitons to use later    
+function celsiusToFahrenheit (input){
+    let fahrenheit =(input * 1.8) + 32;
+    return Math.round(fahrenheit);
+};
+
+function fahrenheiToCelsius (input){
+    let celsius = (input - 32) * 5 / 9;
+    return Math.round(celsius);
+}
+// ____________________________________________
+// fetch html elements to jS to enable from elements
+var userInput = document.getElementById('difficult');
+var submitButton = document.getElementById('submit');
+var clearButton = document.getElementById('clear')
+var screen = document.querySelector('.screen');
+// enable user input and submission button
+//___________________________________________
+submitButton.onclick = function(){
+    
+    console.log(shotCaller);
+    if(shotCaller==="b"){
+    var yourInput = Math.round(userInput.value);
+    const printResult = /* based on path outcome */celsiusToFahrenheit(yourInput);
+    screen.innerHTML = printResult+ "&#8457";
+    }else{
+        var yourInput = Math.round(userInput.value);
+        const printResult = /* based on path outcome */fahrenheiToCelsius(yourInput);
+        screen.innerHTML = printResult+ "&#8451"; 
+    }
+}
+// not able to switch between Celsius and Fahrenheit - unable to retrive the shotCaller value...
+// enable user input and clear button
+//___________________________________
+clearButton.onclick = function(){
+    screen.innerHTML ="";
+    userInput.value="";
+}
+    
+
+
+// ----------------------------------------
